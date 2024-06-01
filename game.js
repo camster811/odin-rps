e9ff42ff515e0e331eb90cad3ce9ccd4eb2816ec
Camster811 <cls0150@auburn.edu>
2024-06-01 01:50:16 -0500

    Create logic for computer choice

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let choice = "yo";
+
+    let randomNum = Math.random();
+    console.log(randomNum);
+
+    switch (true) {
+        case (randomNum < .33):
+            choice = "Rock";
+            break;
+        case (randomNum < 0.66 && randomNum > 0.33):
+            choice = "paper";
+            break;
+        case (0.66 < randomNum):
+            choice = "scissors";
+            break;
+        default:
+            break;
+    }
+    return choice;
+}
+
+console.log(getComputerChoice())
