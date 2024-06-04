5fa1ab1cf14da8549ed60ca072bf09adffe8742b
Camster811 <cls0150@auburn.edu>
2024-06-04 18:30:18 -0500

    Add buttons to control RPS game

--- game.js
+++ game.js
@@ -40,59 +40,65 @@ function getHumanChoice() {
     }
 }
 
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        switch (humanChoice.toLowerCase()) {
-            case "rock":
-                if (computerChoice.toLowerCase() == "paper") {
-                    console.log("Lost Round: Paper beats rock.")
-                    ++computerScore;
-                } else if (computerChoice.toLowerCase() == "scissors") {
-                    console.log("Won round: Rock beats scissors.")
-                    ++humanScore;
-                } else {
-                    console.log("It's a tie!")
-                }
-                break;
-            case "paper":
-                if (computerChoice.toLowerCase() == "rock") {
-                    console.log("Won round: Paper beats rock.")
-                    ++humanScore;
-                } else if (computerChoice.toLowerCase() == "scissors") {
-                    console.log("Lost Round: Scissors beats paper.")
-                    ++computerScore;
-                } else {
-                    console.log("It's a tie!")
-                }
-                break;
-            case "scissors":
-                if (computerChoice.toLowerCase() == "rock") {
-                    console.log("Lost Round: Rock beats scissors.")
-                    ++computerScore;
-                } else if (computerChoice.toLowerCase() == "paper") {
-                    console.log("Won round: Scissors beats paper.")
-                    ++humanScore;
-                } else {
-                    console.log("It's a tie!")
-                }
-                break;
-            default:
-                break;
-        }
-    }
 
-    while (computerScore + humanScore < 5) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-
-    if (humanScore > computerScore) {
-        alert(`You win ${humanScore}-${computerScore}`);
-    } else if (computerScore > humanScore) {
-        alert(`You lose ${humanScore}-${computerScore}`);
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice.toLowerCase()) {
+        case "rock":
+            if (computerChoice.toLowerCase() == "paper") {
+                console.log("Lost Round: Paper beats rock.")
+                ++computerScore;
+            } else if (computerChoice.toLowerCase() == "scissors") {
+                console.log("Won round: Rock beats scissors.")
+                ++humanScore;
+            } else {
+                console.log("It's a tie!")
+            }
+            break;
+        case "paper":
+            if (computerChoice.toLowerCase() == "rock") {
+                console.log("Won round: Paper beats rock.")
+                ++humanScore;
+            } else if (computerChoice.toLowerCase() == "scissors") {
+                console.log("Lost Round: Scissors beats paper.")
+                ++computerScore;
+            } else {
+                console.log("It's a tie!")
+            }
+            break;
+        case "scissors":
+            if (computerChoice.toLowerCase() == "rock") {
+                console.log("Lost Round: Rock beats scissors.")
+                ++computerScore;
+            } else if (computerChoice.toLowerCase() == "paper") {
+                console.log("Won round: Scissors beats paper.")
+                ++humanScore;
+            } else {
+                console.log("It's a tie!")
+            }
+            break;
+        default:
+            break;
     }
 }
 
-playGame();
 
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+    playerChoice = "rock";
+    computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+    playerChoice = "paper";
+    computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+})
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+    playerChoice = "scissors";
+    computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+})
